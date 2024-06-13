ac66a75f909800eae964d3a5f956de9aaae1dd48
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-13 21:50:42 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int year = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            bw.write(String.valueOf(1));
+        }
+        else{
+            bw.write(String.valueOf(0));
+        }
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
