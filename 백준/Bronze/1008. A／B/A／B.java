03f45c542e1ebccb6a30444a7c3ca54ce3a5c08f
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-13 20:21:47 +0900

    [Bronze V] Title: A/B, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,24 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        double div = (double)num1 / num2;
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(div));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
