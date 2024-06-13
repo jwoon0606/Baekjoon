7d993f9f137fd2e5a0cf0b9606f4895430eaa28c
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-13 21:45:07 +0900

    [Bronze V] Title: 시험 성적, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        if(score >= 90)
+            bw.write("A");
+        else if(score >= 80)
+            bw.write("B");
+        else if(score >= 70)
+            bw.write("C");
+        else if(score >= 60)
+            bw.write("D");
+        else
+            bw.write("F");
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
