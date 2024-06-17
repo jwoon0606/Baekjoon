b7641ad36e5174b42ea99b8db60daa3bbe626665
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-17 13:38:42 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException{
-        String str = "abcdefghijklmnopqrstuvwxyz";
+        int[] a = new int[26];
+        Arrays.fill(a, -1);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            int ch = str.charAt(i) - 'a';
+            if(a[ch] == -1)
+                a[ch] = i;
+        }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            int n = -1;
-            for (int j = 0; j < S.length(); j++) {
-                if(str.charAt(i) == S.charAt(j)){
-                    n = j;
-                    break;
-                }
-            }
-            sb.append(n).append(" ");
+        for (int i : a) {
+            sb.append(i).append(" ");
         }
         sb.append("\n");
-
         System.out.print(sb);
     }
 }
