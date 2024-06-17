244cb4bb8accf7afc978bb4811c476ea3cd2b9ee
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-06-17 14:23:05 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n").append("r`-_   ,'  ,/\n").append(" \\. \". L_r'\n").append("   `~\\/\n").append("      |\n").append("      |\n");
+        System.out.print(sb);
+    }
+}
