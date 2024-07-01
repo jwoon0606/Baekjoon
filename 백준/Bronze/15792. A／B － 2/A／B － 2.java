ac2501410bacd2c93fac55dbc745f571b309d249
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-01 13:16:46 +0900

    [Bronze I] Title: A/B - 2, Time: 180 ms, Memory: 17704 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.java
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.StringTokenizer;
 
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        double N = Double.parseDouble(st.nextToken());
-        double M = Double.parseDouble(st.nextToken());
-        System.out.println(N / M);
+        BigDecimal N = new BigDecimal(st.nextToken());
+        BigDecimal M = new BigDecimal(st.nextToken());
+        System.out.println(N.divide(M,10001,BigDecimal.ROUND_DOWN));
     }
 }
