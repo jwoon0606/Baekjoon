ce17107523e5dcc70806743ebbd62b590f356f94
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-02 22:02:59 +0900

    [level 2] Title: 스킬트리, Time: 9.05 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for(int i=0; i<skill_trees.length; i++){
+            String tree = skill_trees[i].replaceAll("[^" + skill + "]","");
+            for(int j=0; j<skill.length() + 1; j++){
+                String sub = skill.substring(0,j);
+                if(tree.equals(sub)){
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
