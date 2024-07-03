e2465066d46572f312b71c9d6710e1353b0ec593
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-03 19:33:57 +0900

    [Silver II] Title: 스택 수열, Time: 360 ms, Memory: 26896 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
         int ready_num = 1;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            int n = scanner.nextInt();
+            int n = Integer.parseInt(br.readLine());
             if(n >= ready_num){
                 while(ready_num <= n) {
                     stack.push(ready_num++);
