80645d4567d0cd5b73797df3a86ef126337d1057
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-04 22:36:36 +0900

    [Bronze V] Title: 아!, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        if(s1.length() < s2.length())
+            System.out.println("no");
+        else
+            System.out.println("go");
+    }
+}
