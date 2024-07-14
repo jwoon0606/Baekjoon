13c8ca1d9611af409585884e518f9ea1339b0e56
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-14 20:54:05 +0900

    [Bronze III] Title: 문문문, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/17210. 문문문/문문문.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        int z = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        if(N >= 6){
+            System.out.println("Love is open door");
+            return;
+        }
+        for (int i = 0; i < N-1; i++) {
+            if(z == 0)
+                sb.append(++z).append("\n");
+            else
+                sb.append(--z).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
