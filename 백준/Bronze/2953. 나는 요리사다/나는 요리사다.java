7e749e82c4b42acf993150121eb209a7ee8cef63
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-14 21:16:32 +0900

    [Bronze III] Title: 나는 요리사다, Time: 120 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[] sum = new int[5];
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                sum[i] += Integer.parseInt(st.nextToken());
+            }
+        }
+        int max = 0;
+        int maxi = 0;
+        for (int i = 0; i < 5; i++) {
+            if (sum[i] > max) {
+                maxi = i + 1;
+                max = sum[i];
+            }
+        }
+        System.out.println(maxi + " " + max);
+    }
+}
