b3b8629cbb87b994cdeda6562f0ea2fa0a05105c
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-14 21:58:59 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = br.readLine().split("");
+        Arrays.sort(strings, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            sb.append(string);
+        }
+        sb.append("\n");
+        System.out.print(sb);
+    }
+}
