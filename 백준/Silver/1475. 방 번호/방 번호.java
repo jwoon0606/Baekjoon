d07eb5adc3786868169a31d1588031d1dded6f96
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-14 19:34:21 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] count = new int[10];
+        char[] input = br.readLine().toCharArray();
+        for (char c : input) {
+            int a = c - '0';
+            count[a]++;
+        }
+        count[6] = (count[6] + count[9]) / 2 + (count[6] + count[9]) % 2;
+
+        int max = 0;
+        for (int i=0; i<9; i++) {
+            int a = count[i];
+            max = Math.max(max,a);
+        }
+
+        System.out.println(max);
+    }
+}
