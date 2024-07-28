364acf3655bc59e1be63eaab08c22667956a1f10
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-07-28 22:46:03 +0900

    [Bronze II] Title: 카이사르 암호, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String before = br.readLine();
+        char[] after = before.toCharArray();
+        for (int i = 0; i < before.length(); i++) {
+            char word = after[i];
+            if (word >= 'A' && word <= 'C') {
+                after[i] = (char) (word + 23);
+            } else {
+                after[i] = (char) (word - 3);
+            }
+        }
+
+        System.out.print(after);
+    }
+}
