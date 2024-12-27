514f3f69fdaeabed512ac99c6fe2ebeb02d23ac9
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-12-27 00:21:41 -0800

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("강한친구 대한육군").append("\n").append("강한친구 대한육군");
+        System.out.println(sb);
+    }
+}
