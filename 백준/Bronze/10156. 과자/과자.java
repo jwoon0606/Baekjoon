c6f0ff1c4cc3e3e9d3d9b0e41f13af4f6809a41e
Woon <162769592+jwoon0606@users.noreply.github.com>
2024-12-27 05:41:15 -0800

    [Bronze IV] Title: 과자, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = K * N - M;
+        if(result < 0) result = 0;
+
+        System.out.println(result);
+    }
+}
