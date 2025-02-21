e307ee273251281f974bed85c0cdef5522a34301
Woon <162769592+jwoon0606@users.noreply.github.com>
2025-02-21 23:20:00 +0900

    [Bronze IV] Title: 입실 관리, Time: 100 ms, Memory: 14208 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(br.readLine().toLowerCase()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
