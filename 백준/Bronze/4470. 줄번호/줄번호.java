343fc7f8ac149cdf30fabc3647e79029e000c0a4
Woon <162769592+jwoon0606@users.noreply.github.com>
2025-02-21 22:04:05 +0900

    [Bronze IV] Title: 줄번호, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            sb.append(i+1).append(". ").append(br.readLine()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
