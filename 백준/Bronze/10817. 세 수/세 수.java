4d0b64535b4cba70af728a902e8fb691f3bb3c6b
Woon <162769592+jwoon0606@users.noreply.github.com>
2025-02-22 02:01:06 +0900

    [Bronze III] Title: 세 수, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -6,18 +7,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        if(A >= B) {
-            if(B >= C) System.out.println(B);
-            else if(A >= C) System.out.println(C);
-            else System.out.println(A);
-        } else {
-            if(A >= C) System.out.println(A);
-            else if(B >= C) System.out.println(C);
-            else System.out.println(B);
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
     }
 }
