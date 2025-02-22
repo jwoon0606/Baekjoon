6651beef98395bfbbe904b1ba04ee302b6e9d019
Woon <162769592+jwoon0606@users.noreply.github.com>
2025-02-22 18:52:48 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(" ".repeat(i));
+            sb.append("*".repeat(2*(N-i)-1));
+            sb.append("\n");
+        }
+
+        for (int i = 1; i < N; i++) {
+            sb.append(" ".repeat(N-i-1));
+            sb.append("*".repeat(i*2+1));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
